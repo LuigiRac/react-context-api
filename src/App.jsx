b4d852f851f07fc7245ce92c163e62c5ccfbb02e
LuigiRac <lui.rac06@gmail.com>
2025-01-19 12:53:08 +0100

    definita lista post all'interno di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,20 @@ function App() {
 
   return (
     <>
+      <h1>Post List</h1>
+      <ul>
 
+        {posts.map((posts) => (
+          <li key={posts.id}>
+            <h2>{posts.title}</h2>
+            <p>{posts.content}</p>
+            <p>{posts.tags.join(', ')}</p>
+          </li>
+
+        )
+        )}
+
+      </ul>
     </>
   )
 }
