12af229e7f2216c007df2398702f8b12193c66ab
LuigiRac <lui.rac06@gmail.com>
2025-01-19 12:25:24 +0100

    import GlobalContext in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { GlobalContext } from './context/GlobalContext';
 
 function App() {
-
+  const posts = [
+    {
+      id: 1,
+      title: "Titolo del Post",
+      image: '',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html", "css"],
+      published: true,
+    },
+    {
+      id: 2,
+      title: "Titolo del Post",
+      image: '',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "css"],
+      published: true,
+    },
+    {
+      id: 3,
+      title: "Titolo del Post",
+      image: '',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["js", "php"],
+      published: true,
+    },
+    {
+      id: 4,
+      title: "Titolo del Post",
+      image: '',
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+      tags: ["html"],
+      published: false,
+    },
+  ];
 
   return (
     <>
